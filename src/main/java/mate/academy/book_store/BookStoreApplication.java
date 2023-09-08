50a4a91ea3a3b205c6405f58b779183095f877a2
Tim <tymurprokhorov@gmail.com>
2023-09-09 00:10:22 +0300

    Spring boot app configured, book-entity, book-repository and book-service implemented

--- src/main/java/mate/academy/book_store/BookStoreApplication.java
+++ src/main/java/mate/academy/book_store/BookStoreApplication.java
@@ -1,13 +1,40 @@
 package mate.academy.book_store;
 
+import mate.academy.book_store.model.Book;
+import mate.academy.book_store.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import java.math.BigDecimal;
 
 @SpringBootApplication
 public class BookStoreApplication {
+    BookService bookService;
+
+    @Autowired
+    public BookStoreApplication(BookService bookService) {
+        this.bookService = bookService;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(BookStoreApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner commandLineRunner() {
+        return args -> {
+            Book book = new Book();
+            book.setAuthor("Stephen King");
+            book.setIsbn("1234");
+            book.setPrice(BigDecimal.valueOf(1000));
+            book.setDescription("Horror novel about monsters");
+            book.setTitle("The mist");
+            book.setCoverImage("coverImageUrl");
+
+            bookService.save(book);
+            System.out.println(bookService.findAll());
+        };
+    }
 }
---
+++ src/main/java/mate/academy/book_store/repository/impl/BookRepositoryImpl.java
@@ -0,0 +1,51 @@
+package mate.academy.book_store.repository.impl;
+
+import mate.academy.book_store.model.Book;
+import mate.academy.book_store.repository.BookRepository;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public class BookRepositoryImpl implements BookRepository {
+    private final SessionFactory sessionFactory;
+
+    @Autowired
+    public BookRepositoryImpl(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    @Override
+    public Book save(Book book) {
+        Transaction transaction = null;
+        Session session = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.persist(book);
+            transaction.commit();
+            return book;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can't save a book: " + book, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    @Override
+    public List<Book> findAll() {
+        try (Session session = sessionFactory.openSession()) {
+            return session.createQuery("SELECT b FROM Book b", Book.class).getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException("Can`t find books from DB.", e);
+        }
+    }
+}
