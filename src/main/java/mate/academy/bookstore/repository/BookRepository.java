78ccb54e9c8b0359101d4cbaa3a8259b0d80b25f
Tim <tymurprokhorov@gmail.com>
2023-09-19 15:21:48 +0300

    BookRepositoryImpl removed in favor of JpaRepository

--- src/main/java/mate/academy/bookstore/repository/BookRepository.java
+++ src/main/java/mate/academy/bookstore/repository/BookRepository.java
@@ -1,13 +1,7 @@
 package mate.academy.bookstore.repository;
 
-import java.util.List;
-import mate.academy.bookstore.dto.BookDto;
 import mate.academy.bookstore.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BookRepository {
-    Book save(Book book);
-
-    List<Book> findAll();
-
-    BookDto getBookById(Long id);
+public interface BookRepository extends JpaRepository<Book, Long> {
 }
--- src/main/java/mate/academy/bookstore/service/impl/BookServiceImpl.java
+++ src/main/java/mate/academy/bookstore/service/impl/BookServiceImpl.java
@@ -1,15 +1,16 @@
 package mate.academy.bookstore.service.impl;
 
-import java.util.List;
-import java.util.Random;
 import lombok.RequiredArgsConstructor;
 import mate.academy.bookstore.dto.BookDto;
 import mate.academy.bookstore.dto.CreateBookRequestDto;
+import mate.academy.bookstore.exception.EntityNotFoundException;
 import mate.academy.bookstore.mapper.BookMapper;
 import mate.academy.bookstore.model.Book;
 import mate.academy.bookstore.repository.BookRepository;
 import mate.academy.bookstore.service.BookService;
 import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Random;
 
 @RequiredArgsConstructor
 @Service
@@ -33,6 +34,10 @@ public class BookServiceImpl implements BookService {
 
     @Override
     public BookDto getBookById(Long id) {
-        return bookRepository.getBookById(id);
+        Book book = bookRepository.findById(id).orElseThrow(
+                () -> new EntityNotFoundException("Can`t find book by id: " + "id"));
+        return bookMapper.toDto(book);
     }
+
+
 }
