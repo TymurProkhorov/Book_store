eeadee6b3309e671d8130ae60941cb6b861936c1
Tim <tymurprokhorov@gmail.com>
2023-09-29 17:49:40 +0300

    annotations above some repositories added

--- src/main/java/mate/academy/bookstore/service/impl/UserServiceImpl.java
+++ src/main/java/mate/academy/bookstore/service/impl/UserServiceImpl.java
@@ -12,9 +12,9 @@ import mate.academy.bookstore.service.UserService;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
-@Component
+@Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
